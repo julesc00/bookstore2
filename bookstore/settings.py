d900c244a91f79ebe898d4fbb401a8077cb6d3df
Julio Briones <julesc00@protonmail.com>
2024-11-03 15:01:21 -0600

    Finished ch5

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
     # Local apps
     "accounts.apps.AccountsConfig",
+    "pages.apps.PagesConfig",
 ]
 
 AUTH_USER_MODEL = "accounts.CustomUser"
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'bookstore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ tests/pages/tests.py
@@ -0,0 +1,35 @@
+from http import HTTPStatus
+
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+
+from pages.views import HomePageView
+
+
+class HomepageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse("pages:home")
+        self.res = self.client.get(url)
+
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_homepage_url_name(self):
+        self.assertEqual(self.res.status_code, HTTPStatus.OK)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.res, "pages/home.html")
+
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(self.res, "This is the Home page")
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.res, "Hi there!")
+
+    def test_homepage_url_resolve_home_page_view(self):
+        view = resolve("/")
+        self.assertEqual(
+            view.func.__name__,
+            HomePageView.as_view().__name__
+        )
